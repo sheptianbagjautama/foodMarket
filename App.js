1dfa7078b279b047cd2b2282860cdde4d858351f
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-29 22:47:51 +0700

    Change App Tsx to JS file

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>App Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
