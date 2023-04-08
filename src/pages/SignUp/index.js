5269dab2919c776531268f94d1fc9846221bfc3d
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-04-09 01:31:52 +0700

    Setup Upload Photo

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,17 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React, {useState} from 'react';
 import {Button, Gap, Header, TextInput} from '../../components';
 import {useDispatch, useSelector} from 'react-redux';
-import {useForm} from '../../utils';
+import {showMessage, useForm} from '../../utils';
 import {setRegister} from '../../redux/reducer/registerSlice';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 const SignUp = ({navigation}) => {
   const [form, setForm] = useForm({
@@ -12,11 +20,41 @@ const SignUp = ({navigation}) => {
     password: '',
   });
 
+  const [photo, setPhoto] = useState('');
+
   const dispatch = useDispatch();
 
   const onSubmit = () => {
     dispatch(setRegister(form));
     navigation.navigate('SignUpAddress');
+  };
+
+  const addPhoto = () => {
+    launchImageLibrary(
+      {
+        quality: 0.5,
+        maxWidth: 200,
+        maxHeight: 200,
+        saveToPhotos: true,
+      },
+      response => {
+        console.log('response = ', response);
+
+        if (response.didCancel || response.errorCode) {
+          showMessage('Anda tidak memilih photo');
+        } else {
+          const source = {uri: response.assets[0].uri};
+          console.log('source', source);
+          const dataImage = {
+            uri: response.assets[0].uri,
+            type: response.assets[0].type,
+            name: response.assets[0].fileName,
+          };
+
+          setPhoto(source);
+        }
+      },
+    );
   };
 
   return (
@@ -25,11 +63,17 @@ const SignUp = ({navigation}) => {
         <Header title="Sign Up" subTitle="Register and eat" onBack={() => {}} />
         <View style={styles.container}>
           <View style={styles.photo}>
-            <View style={styles.borderPhoto}>
-              <View style={styles.photoContainer}>
-                <Text style={styles.addPhoto}>Add Photo</Text>
+            <TouchableOpacity onPress={addPhoto}>
+              <View style={styles.borderPhoto}>
+                {photo ? (
+                  <Image source={photo} style={styles.photoContainer} />
+                ) : (
+                  <View style={styles.photoContainer}>
+                    <Text style={styles.addPhoto}>Add Photo</Text>
+                  </View>
+                )}
               </View>
-            </View>
+            </TouchableOpacity>
           </View>
           <TextInput
             label="Full Name"
@@ -93,7 +137,8 @@ const styles = StyleSheet.create({
     height: 90,
     backgroundColor: '#F0F0F0',
     borderRadius: 90,
-    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   addPhoto: {
     fontSize: 14,
